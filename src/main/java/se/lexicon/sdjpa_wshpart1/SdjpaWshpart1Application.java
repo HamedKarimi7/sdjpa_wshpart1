f38577160d57bed18e0ab2ff9604c5bf79eaf747
Hamed Karimi <hmdkrm.rtj@gmail.com>
2022-06-21 12:34:59 +0200

    create project

---
+++ src/main/java/se/lexicon/sdjpa_wshpart1/SdjpaWshpart1Application.java
@@ -0,0 +1,13 @@
+package se.lexicon.sdjpa_wshpart1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdjpaWshpart1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SdjpaWshpart1Application.class, args);
+    }
+
+}
---
+++ src/test/java/se/lexicon/sdjpa_wshpart1/SdjpaWshpart1ApplicationTests.java
@@ -0,0 +1,13 @@
+package se.lexicon.sdjpa_wshpart1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdjpaWshpart1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
