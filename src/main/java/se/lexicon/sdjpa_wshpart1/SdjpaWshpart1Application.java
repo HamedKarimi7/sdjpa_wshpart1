9bebd06b1a728a310292a89e924034b1cc104c61
Hamed Karimi <hmdkrm.rtj@gmail.com>
2022-06-21 15:04:54 +0200

    create AppUser class + add auto creat table to app-properties

--- src/main/java/se/lexicon/sdjpa_wshpart1/SdjpaWshpart1Application.java
+++ src/main/java/se/lexicon/sdjpa_wshpart1/SdjpaWshpart1Application.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SdjpaWshpart1Application {
 
-    public static void main(String[] args) {
-        SpringApplication.run(SdjpaWshpart1Application.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(SdjpaWshpart1Application.class, args);
+	}
 
 }
